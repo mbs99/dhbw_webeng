7e52487f91b113499a15c7ab2cf9a6ad8f485276
Marc Ströbel <m.stroebel@gmx.de>
2020-08-28 15:48:20 +0200

    refactored

--- projects/json/jackson-json/src/main/java/de/webeng/Parser.java
+++ projects/json/jackson-json/src/main/java/de/webeng/Parser.java
@@ -2,19 +2,14 @@ package de.webeng;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.webeng.model.Company;
-import de.webeng.model.Staff;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
 
 public class Parser {
     private ObjectMapper mapper = new ObjectMapper();
 
     public Company parse(InputStream jsonStream) throws IOException {
-
-        List<Staff> staff = Arrays.asList(mapper.readValue(jsonStream, Staff[].class));
-        return new Company(staff);
+        return mapper.readValue(jsonStream, Company.class);
     }
 }
--- projects/json/jackson-json/src/main/java/de/webeng/model/Company.java
+++ projects/json/jackson-json/src/main/java/de/webeng/model/Company.java
@@ -1,12 +1,14 @@
 package de.webeng.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class Company {
 
     private List<Staff> staff;
 
-    public Company(List<Staff> staff) {
+    public Company(@JsonProperty("staff") List<Staff> staff) {
         this.staff = staff;
     }
 
